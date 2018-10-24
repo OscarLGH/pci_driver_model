8e146fe2e6155f9ce37abf1c6d62ffcfa0d7a344
liguanhua <ligh429@tsinghua.edu.cn>
2018-10-24 14:20:02 +0800

    Add private driver structure. Add IRQ handler.

--- pci_driver.c
+++ pci_driver.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/pci.h>
+
 #include "pci_driver.h"
 
 static struct pci_device_id
@@ -13,15 +14,58 @@ pci_driver_model_id_table[] = {
 	}	
 };
 
+static irqreturn_t pci_driver_irq(int irq, void *data)
+{
+	irqreturn_t result = IRQ_HANDLED;//IRQ_NONE
+	struct pci_driver_model *drv_data = data;
+	drv_data->irq_cnt++;
+	printk("IRQ:%d\n", irq);
+	return result;
+}
+
+static irqreturn_t pci_driver_irq_check(int irq, void *data)
+{
+	struct pci_driver_model *drv_data = data;
+	printk("IRQ:%d\n", irq);
+	printk("IRQ cnt:%d\n", drv_data->irq_cnt);
+	return IRQ_WAKE_THREAD;//IRQ_NONE
+}
+
 static int pci_driver_model_probe(struct pci_dev *pdev, const struct pci_device_id *pent)
 {
 	u16 vid, did;
-	u32 *bar_ptr[6];
 	u64 bar_base, bar_len;
+	int ret;
+	struct pci_driver_model *drv_data = NULL;
 	int i;
+
+	drv_data = kmalloc(sizeof(*drv_data), GFP_KERNEL);
+	if (drv_data == NULL) {
+		return -ENOMEM;
+	}
+
+	pci_set_drvdata(pdev, drv_data);
+
 	printk("%s\n", __FUNCTION__);
-	pci_enable_device(pdev);
+	ret = pci_enable_device(pdev);
+	if (ret) {
+		printk("enabling pci device failed.\n");
+		return -ENODEV;
+	}
+
 	pci_set_master(pdev);
+
+	ret = pci_enable_msi(pdev);
+	if (ret) {
+		printk("enabling MSI failed.\n");
+	}
+
+	ret = pci_request_irq(pdev, pdev->irq, pci_driver_irq_check, pci_driver_irq, pci_get_drvdata(pdev), "pci_driver_irq %d", pdev->irq);
+	if (ret) {
+		printk("request irq failed.\n");
+		return ret;
+	}
+
 	pci_read_config_word(pdev, 0x0, &vid);
 	pci_read_config_word(pdev, 0x2, &did);
 	printk("PCI vendor ID:%04x, PCI device ID:%04x\n", vid, did);
@@ -30,10 +74,11 @@ static int pci_driver_model_probe(struct pci_dev *pdev, const struct pci_device_
 		bar_len = pci_resource_len(pdev, i);
 		printk("BAR%d:base = 0x%08llx, len = 0x%llx\n", i, bar_base, bar_len);
 		if (pci_resource_start(pdev, i) != 0) {
-			bar_ptr[i] = ioremap(bar_base, bar_len);
-			printk("BAR%d[0x0] = %08x\n", i, bar_ptr[i][0]);
+			drv_data->regs[i] = ioremap(bar_base, bar_len);
+			printk("BAR%d[0x0] = %08x\n", i, drv_data->regs[i][0]);
 		}
 	}
+
 	return 0;
 }
 
--- pci_driver.h
+++ pci_driver.h
@@ -1,3 +1,12 @@
 #define PCI_VENDOR_ID_MODEL 0x8086
 #define PCI_DEVICE_ID_MODEL 0x7110
 #define PCI_MODEL_BASE_CLASS 0x6
+
+struct pci_driver_model {
+	u32 *regs[6];
+	void *oprom;
+	void *dma_buffer_in;
+	void *dma_buffer_out;
+	u32 irq_cnt;
+	u32 reserved;
+};
